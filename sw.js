5b4d94b011b7d2633ad9d1bb504801cded688ba3
YuanXuJia <xuanyuanziruo@gmail.com>
2017-07-11 08:49:00 +0800

    fix method name

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const mainCacheName = 'yuanxj1024-chache';
 this.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.open(mainCacheName).then((cache) => {
-      return cache.add([
+      return cache.addAll([
         '/',
         'index.html',
         'index.js'
